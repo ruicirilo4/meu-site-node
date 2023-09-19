ab54ccb15a9af06f32951ef8f388666aa8bbd706
ruicirilo4 <100714573+ruicirilo4@users.noreply.github.com>
2023-09-19 08:40:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,25 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import express from 'express';
-import puppeteer from 'puppeteer';
+const puppeteer = require("puppeteer");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const app = express();
-const port = 56189;
-
-app.use(express.static(__dirname + '/public'));
-
-// Create an array to store recent songs
-const recentSongs = [];
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/now-playing', async (req, res) => {
+module.exports = async (req, res) => {
   try {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    await page.goto('https://www.antena1.com.br/ouvir-radio-online');
+    await page.goto("https://www.antena1.com.br/ouvir-radio-online");
 
-    await page.waitForSelector('.Artist', { timeout: 180000 });
-    await page.waitForSelector('.nowPlayingLink');
+    await page.waitForSelector(".Artist", { timeout: 180000 });
+    await page.waitForSelector(".nowPlayingLink");
 
-    const artistText = await page.$eval('.Artist', (element) =>
-      element.textContent.trim()
-    );
-    const songText = await page.$eval('.nowPlayingLink', (element) =>
-      element.textContent.trim()
-    );
+    const artistText = await page.$eval(".Artist", (element) => element.textContent.trim());
+    const songText = await page.$eval(".nowPlayingLink", (element) => element.textContent.trim());
 
     await browser.close();
 
     const nowPlayingInfo = `${artistText} - ${songText}`.trim();
 
-    // Check if the song is not already in the recentSongs array,
-    // and it's not "-" or an empty string before adding it
-    if (
-      nowPlayingInfo !== '-' &&
-      nowPlayingInfo !== '' &&
-      !recentSongs.includes(nowPlayingInfo)
-    ) {
-      // Add the currently playing song to the beginning of the recentSongs array
-      recentSongs.unshift(nowPlayingInfo);
-
-      // Ensure the recentSongs array contains a maximum of 10 songs
-      if (recentSongs.length > 10) {
-        recentSongs.pop(); // Remove the oldest song if there are more than 10
-      }
-    }
-
-    res.send(nowPlayingInfo);
+    res.status(200).json({ nowPlayingInfo });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Error');
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error" });
   }
-});
-
-app.get('/recent-songs', (req, res) => {
-  // Filter out "-" and empty string from the recentSongs array
-  const filteredRecentSongs = recentSongs.filter(
-    (song) => song !== '-' && song !== ''
-  );
-
-  // Return the filtered list of recent songs
-  res.json(filteredRecentSongs);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+};
